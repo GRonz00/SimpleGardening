7322a3b8e24a4e37ca4bf84386bfab4b9439c8e7
GRonz00 <92393572+GRonz00@users.noreply.github.com>
2023-12-02 18:13:53 +0100

    home gui

--- src/main/java/com/example/simplegardening/SimpleGardeningApplication.java
+++ src/main/java/com/example/simplegardening/SimpleGardeningApplication.java
@@ -10,11 +10,11 @@ import java.io.IOException;
 public class SimpleGardeningApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(SimpleGardeningApplication.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(SimpleGardeningApplication.class.getResource("plant.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 280);
         stage.setTitle("Simple Gardening");
-        stage.setMinWidth(800);
-        stage.setMinHeight(600);
+        //stage.setMinWidth(800);
+        //stage.setMinHeight(600);
         stage.setScene(scene);
         stage.show();
     }
